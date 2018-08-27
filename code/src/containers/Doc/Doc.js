6f6bcb6cf1f32bd27024fbfba167ebd221a77c95
Aman Singh <aman.info.92@gmail.com>
2018-08-27 10:58:45 +0530

    created file tree in [Doc.js] using react-sortable-tree [See file for link]

--- code/src/containers/Doc/Doc.js
+++ code/src/containers/Doc/Doc.js
@@ -1,79 +1,74 @@
 import React, { Component } from 'react';
 import classes from './Doc.css';
 
+import SortableTree from 'react-sortable-tree'; // https://github.com/frontend-collective/react-sortable-tree
+import FileExplorerTheme from 'react-sortable-tree-theme-file-explorer'; // https://github.com/frontend-collective/react-sortable-tree-theme-file-explorer
+
+import JSIcon from '../../assets/js-icon.png';
+import ImgIcon from '../../assets/img-icon.png';
 class Doc extends Component {
-    constructor(props) {
-        super(props);
-        this.noOfSpaces = 0;
-    }
-
     state = {
-        tree: {
-            'src': [
-                { 'assets': [] },
-                { 'components': [
-                        { 'Choices': [ { 'Choices.js': [] } ] },
-                        { 'Navigation': [ { 'NavigationItems': [ { 'NavigationItems.js': [] }, { 'NavigationItem': [ { 'NavigationItem.js': [] } ] } ] }, { 'Toolbar': [ { 'Toolbar.js': [] } ] } ] } 
-                    ]
-                },
-                { 'containers': [
-                        { 'Auth': [ { 'Auth.js': [] } ] }
-                    ]
-                }
-            ]
-        },
-        tree2: {
-            "name": "a",
-            "children": [
-                {
-                    "name": "h",
-                    "children": []
-                },
-                {
-                    "name": "i",
-                    "children": []
-                },
-                {
-                    "name": "j",
-                    "children": []
-                }
-            ]
-        }
-    }
-
-    componentDidMount() {
-        this.createTree(this.state.tree2, this.state.tree2.children);
-    }
-
-    createTree = (tree, noOfChilds) => {
-        let printStr = '';
-        for(let i = 0; i < this.noOfSpaces; i++) {
-            printStr += '-'
-        }
-        printStr += tree.name;
-        console.log(printStr);
-        if(tree.children.length > 0) {
-            this.noOfSpaces++;
-        }
-         for(let j = 0; j < tree.children.length; j++) {
-             if(tree.children[j].length > 0) {
-                this.createTree(tree.children[j], tree.children.length);
-             } else {
-                this.createTree(tree.children[j]);
-             }
-        }
-        this.noOfSpaces--;
+        treeData: [
+            { 
+                title: 'src/',
+                type: "dir",
+                expanded: true,
+                children: [ 
+                    { 
+                        title: 'assets',
+                        type: "file", 
+                    },
+                    {
+                        title: 'components',
+                        expanded: true,
+                        type: "dir",
+                        children: [
+                            {
+                                title: 'Choices',
+                                type: "dir",
+                                children: [
+                                    {
+                                        title: 'Choices.js',
+                                        type: "js",
+                                    }
+                                ]
+                            },
+                            {
+                                title: 'Error404',
+                                type: "dir",
+                                children: [
+                                    {
+                                        type: "js",
+                                        title: 'Error404.js'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ] 
+            }
+        ],
     }
 
     render() {
-
-
         return (
             <div className={classes.Doc}>
                 <p className={classes.Title}>tree</p>
                 <div className={classes.DocCont}>
                     <div className={classes.DocSideBar}>
-
+                        <SortableTree
+                            treeData={this.state.treeData}
+                            onChange={treeData => this.setState({ treeData })}
+                            theme={FileExplorerTheme}
+                            canDrag={false}
+                            generateNodeProps={rowInfo => ({
+                                icons: rowInfo.node.type === "js"
+                                ? [ <img src={JSIcon} alt="JS Icon" /> ]
+                                : rowInfo.node.type === "file"
+                                    ? [ <img src={ImgIcon} alt="Img Icon" /> ]
+                                    : []
+                            })}
+                        />
                     </div>
                     <div className={classes.DocBody}>
                     </div>
